4c874f1c1b9cb5f3c5f3682ae21b3ae9f6cb91bc
haruyasu <harukun2002@gmail.com>
2019-07-19 21:34:11 -0400

    Updates

---
+++ precache-manifest.a8f7108e5e828667fde5f4a96a1861a4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c7758b3921558441c73ea5765554d0d",
+    "url": "/recipe-react/index.html"
+  },
+  {
+    "revision": "513eeb2fda7af59238af",
+    "url": "/recipe-react/static/css/main.432e64bf.chunk.css"
+  },
+  {
+    "revision": "e269caec8435623e6b29",
+    "url": "/recipe-react/static/js/2.881939cd.chunk.js"
+  },
+  {
+    "revision": "513eeb2fda7af59238af",
+    "url": "/recipe-react/static/js/main.0ea885fd.chunk.js"
+  },
+  {
+    "revision": "665fcce085e729b26044",
+    "url": "/recipe-react/static/js/runtime~main.f54b8cf1.js"
+  }
+]);
